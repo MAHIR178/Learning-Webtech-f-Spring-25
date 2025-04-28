e730440c41d27c3904783479a0710dc66e656e39
Mahir Shariar Ratul <mahirratul2002@gmail.com>
2025-04-28 14:56:44 +0600

    Add files via upload

---
+++ prac-01.php
@@ -0,0 +1,11 @@
+<?php
+//$length=10;
+//$width=5;
+//$area=$length*$width;
+//$perimeter=2*($length+$width);
+//echo"".$area."";
+//echo "".$perimeter."";
+$amount=1000;
+$vat=0.15 * $amount;
+echo "vat on $amount is $vat <br>;
+?>
